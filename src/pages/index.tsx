8aa22bae32c106d4d85ac6bec1a317a4c204f91c
Freddie Mixell <fmixell@gmail.com>
2022-06-20 11:45:14 -0400

    prefer date.now over instance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -464,7 +464,7 @@ const Index = () => {
       <div className="bg-purple-300 shadow">
         <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
           <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-            <p className="text-white">last updated: {new Date().getTime()}</p>
+            <p className="text-white">last updated: {Date.now()}</p>
           </div>
         </div>
       </div>
