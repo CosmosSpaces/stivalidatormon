813c1129562435ccb82de606f7140e3b5ab726d2
Freddie Mixell <fmixell@gmail.com>
2022-06-21 11:47:43 -0400

    info update

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -13,43 +13,43 @@ const features = [
   {
     name: 'Purpose',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      'This is a simple application for operators of validators to watch their collection of validators across multiple chains, in a one screen snapshot using open chain information, unless the "view secure info" switch is toggled. That will required whitelisting who can see that additional information on the validator itself.',
     icon: TerminalIcon,
-  },
-  {
-    name: 'Support Cosmos',
-    description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: HeartIcon,
   },
   {
     name: 'Join',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      'A team dedicated to the educations, technical, community building of the Cosmos Ecosystem.',
     icon: RefreshIcon,
+  },
+  {
+    name: 'Support Cosmos',
+    description:
+      'Our mission of education, technology, and community building can only be furthered with the help of the community. From participating in our spaces, to, kind words, and delegation to our validators. You choose what works for you! Further info for these things is available here: <a href="https://linktr.ee/Cosmostwitterspaces">https://linktr.ee/Cosmostwitterspaces</a>',
+    icon: HeartIcon,
   },
   {
     name: 'Privacy',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      'This application collects no personal information at any time. It is a simple block explorer.',
     icon: ShieldCheckIcon,
   },
   {
     name: 'Developers',
     description:
-      'A collaboration from the minds of Marty Schoffstall & Freddie Mixell.',
+      'A collaboration from the minds of Freddie Mixell & Marty Schoffstall.',
     icon: CogIcon,
   },
   {
     name: 'Thanks',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      'Special thanks to the Cosmos ecosystem and all the developers who have helped by figuring out how to work with this open block data.',
     icon: ServerIcon,
   },
   {
     name: 'Coming Soon',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      "Alpha version v1.0 projected for July of 2022. What you're currently viewing is a beta version.",
     icon: ServerIcon,
   },
 ];
@@ -73,9 +73,10 @@ export default function Info() {
                 <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
                   {feature.name}
                 </h3>
-                <p className="mt-5 text-base text-gray-500">
-                  {feature.description}
-                </p>
+                <p
+                  className="mt-5 text-base text-gray-500"
+                  dangerouslySetInnerHTML={{ __html: feature.description }}
+                />
               </div>
             </div>
           </div>
